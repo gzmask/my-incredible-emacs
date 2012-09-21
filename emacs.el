86014dfee7dfaf8a7c464e3e894cda43ee110cfd
Shulang Lei <gzmask@Shulangs-MacBook-Air.local>
2012-09-21 00:58:37 -0600

    minor

--- emacs.el
+++ emacs.el
@@ -1,13 +1,12 @@
 ; theme
 (load-theme 'manoj-dark t)
 
-; marmalade
+; marmalade melpa
 (require 'package)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
-; melpa
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 ; evil plugin
 (add-to-list 'load-path "~/.emacs.d/evil")
@@ -35,15 +34,3 @@
 ; indent
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
