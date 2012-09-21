de955e0dce3bf8a42b51f42564e6c80bab5da681
gzmask <gzmask@barb.(none)>
2012-09-21 00:19:40 -0600

    added clojuremode nrepl

--- emacs.el
+++ emacs.el
@@ -1,6 +1,15 @@
 ; theme
 (load-theme 'manoj-dark t)
 
+; marmalade
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+; melpa
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
 ; evil plugin
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
@@ -27,3 +36,15 @@
 ; indent
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
